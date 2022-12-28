601f31e89c8bfacc88acf37ce9e18591c3685d98
abdelilahagouni <abdelilah.agouni@gmail.com>
2022-12-28 22:25:03 +0300

    Scaffold editing

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.light(),
-      home: Salam saadaoui ,
+      home: Scaffold(),
       
       
       
